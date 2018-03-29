b0858b7bbe2be800f2f5243fdc325e014eb05455
Luca Cozzuto <lucacozzuto@MacBook-Air-de-Carolina.local>
2018-03-29 20:09:54 +0200

    adding extra params

--- lib/BiologicalFunctions.groovy
+++ lib/BiologicalFunctions.groovy
@@ -11,7 +11,7 @@
 	 * Function for mapping PE reads with STAR mapper. It reads both gzipped and plain fastq
  	 */
 	
-    static String mappingPairsWithSTAR( pair_id, STARgenome, reads, cpus, debug="no") { 
+    static String mappingPairsWithSTAR( pair_id, STARgenome, reads, cpus, extrapars="", debug="no") { 
         """
         	if [ `echo ${debug} == "debug"` ]; then print="echo "; else print=""; fi
 			if [ `echo ${reads} | grep ".gz"` ]; then gzip="--readFilesCommand zcat"
@@ -23,7 +23,8 @@
                      --outSAMunmapped None \
                      --outSAMtype BAM SortedByCoordinate \
                      --runThreadN ${cpus} \
-                     --quantMode GeneCounts \
+                     --quantMode GeneCounts \ 
+                     ${extrapars} \
                      --outFileNamePrefix ${pair_id}
 
                 \$print mkdir STAR_${pair_id}
@@ -39,10 +40,10 @@
 	 * Function for trimming read pairs with Skewer
  	 */
 	
-    static def trimPairsWithSkewer( pair_id, reads, minread, cpus, debug="no") { 
+    static def trimPairsWithSkewer( pair_id, reads, minread, cpus, extrapars="", debug="no") { 
 	"""
 		if [ `echo ${debug} == "debug"` ]; then print="echo "; else print=""; fi
-		\$print	skewer -t ${cpus} -l ${minread} -n -u -o ${pair_id} -z ${reads}
+		\$print	skewer ${extrapars} -t ${cpus} -l ${minread} -n -u -o ${pair_id} -z ${reads}
     	"""
         .stripIndent()
      }
@@ -52,10 +53,10 @@
 	 * Function for mapping single ends with Bowtie2. It returns a bam file ${read_id}.bam (conversion via samtools)
  	 */
 	
-    static def mappingSingleEndsWithBowtie2(read_id, single_reads, index, cpus, debug="no") { 
+    static def mappingSingleEndsWithBowtie2(read_id, single_reads, index, cpus, extrapars="", debug="no") { 
 	"""
 		if [ `echo ${debug} == "debug"` ]; then print="echo "; else print=""; fi
-		\$print	"bowtie2 --non-deterministic -x ${index} -U ${single_reads} -p ${cpus} | samtools view -Sb -@ ${cpus} - > ${read_id}.bam"
+		\$print	"bowtie2 --non-deterministic ${extrapars} -x ${index} -U ${single_reads} -p ${cpus} | samtools view -Sb -@ ${cpus} - > ${read_id}.bam"
     """
         .stripIndent()
      }
@@ -64,11 +65,11 @@
 	 * Function for indexing a genome via Bowtie1. It returns an index composed by a number of files genome_bowtie1*  
 	 * It reads both gzipped and plain fastq
  	 */
-    static def indexingGenomeWithBowtie1(genomeFile, single_reads, index, cpus, debug="no") { 
+    static def indexingGenomeWithBowtie1(genomeFile, single_reads, index, cpus, extrapars="", debug="no") { 
 	"""
 		if [ `echo ${debug} == "debug"` ]; then print="echo "; else print=""; fi
 		if [ `echo ${genomeFile} | grep 'gz'` ]; then \$print "zcat ${genomeFile} > genome_bowtie1.fa"; else \$print ln -s ${genomeFile} genome_bowtie1.fa; fi
-		\$print bowtie-build --threads ${cpus} genome_bowtie1.fa genome_bowtie1
+		\$print bowtie-build ${extrapars} --threads ${cpus} genome_bowtie1.fa genome_bowtie1
 		\$print rm genome_bowtie1.fa
     """
         .stripIndent()
