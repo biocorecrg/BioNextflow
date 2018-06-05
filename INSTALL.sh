47099fae80f635fb36c19db440991a5718755121
Luca Cozzuto <lucacozzuto@gmail.com>
2018-06-05 16:00:34 +0200

    adding INSTALL

---
+++ INSTALL.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Check pipeline dependency
+bionext_ver="0.3"
+
+# Check presence of a lib
+if [ -d "lib" ]; then {
+	echo "BioNextflow installed"
+} else {
+  		echo "BioNextflow not installed. Installing it..."
+		wget https://github.com/biocorecrg/BioNextflow/archive/${bionext_ver}.tar.gz
+		tar -zvxf ${bionext_ver}.tar.gz
+		mv "BioNextflow-${bionext_ver}/lib" .
+		rm ${bionext_ver}.tar.gz
+		rm -fr BioNextflow-${bionext_ver}
+}
+fi
+
+
+
