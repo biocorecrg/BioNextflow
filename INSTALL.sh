e9b789c27bdaaea2b92f29bd7856703675a4b02b
Luca Cozzuto <lucacozzuto@gmail.com>
2018-09-26 10:39:50 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,12 +7,12 @@ bionext_ver="0.3"
 if [ -d "lib" ]; then {
 	echo "BioNextflow installed"
 } else {
-  		echo "BioNextflow not installed. Installing it..."
-		wget https://github.com/biocorecrg/BioNextflow/archive/${bionext_ver}.tar.gz
-		tar -zvxf ${bionext_ver}.tar.gz
-		mv "BioNextflow-${bionext_ver}/lib" .
-		rm ${bionext_ver}.tar.gz
-		rm -fr BioNextflow-${bionext_ver}
+  	echo "BioNextflow not installed. Installing it..."
+	wget https://github.com/biocorecrg/BioNextflow/archive/${bionext_ver}.tar.gz
+	tar -zvxf ${bionext_ver}.tar.gz
+	mv "BioNextflow-${bionext_ver}/lib" .
+	rm ${bionext_ver}.tar.gz
+	rm -fr BioNextflow-${bionext_ver}
 }
 fi
 
